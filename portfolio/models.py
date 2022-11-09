cc559190e831a1c7dcb47b28fadd9eeb707aaec3
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-11-09 19:01:17 +0100

    español

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
